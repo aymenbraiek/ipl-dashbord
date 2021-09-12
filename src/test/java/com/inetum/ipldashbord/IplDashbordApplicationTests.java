4bf4fa3b506dbc269756441c21b8c5fb9eab343a
aymenbraiek <aymen.braiek@esprit.tn>
2021-09-12 19:14:05 +0100

    Intial Project closes #1

---
+++ src/test/java/com/inetum/ipldashbord/IplDashbordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.ipldashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashbordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
